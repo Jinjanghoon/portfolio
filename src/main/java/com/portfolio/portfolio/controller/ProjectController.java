30343eafd80d2b70a54fb963ef389fd8b404dffc
Jinjanghoon <wkdgns3030@naver.com>
2023-04-01 21:20:45 +0900

    인텔리제이 프로젝트_1

--- src/main/java/com/portfolio/portfolio/controller/ProjectController.java
+++ src/main/java/com/portfolio/portfolio/controller/ProjectController.java
@@ -1,8 +1,14 @@
 package com.portfolio.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProjectController {
 
+  @GetMapping("test")
+  public String Test() {
+
+    return "test";
+  }
 }
